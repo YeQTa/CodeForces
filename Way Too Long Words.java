04b423163c708576c82bb95b0e7dd9fdf3df1d35
Yekta Anıl <YektaAnilAksoy@users.noreply.github.com>
2017-03-13 21:27:49 +0200

    Create Way Too Long Words.java

---
+++ Way Too Long Words.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Problem Link = http://codeforces.com/problemset/problem/71/A
+ * @author YektaAnıl
+ */
+public class Main {
+
+
+public static void main(String arg[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    while(n-->=0){
+        String s = sc.nextLine();
+        if(s.length()>10){
+            System.out.println(s.charAt(0)+""+(s.length()-2)+""+s.charAt(s.length()-1));
+        }
+        else
+            System.out.println(s);
+    }
+ }
+}
