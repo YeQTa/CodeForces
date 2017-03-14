f2d696386a764a247786822514ee13cf08f7d454
Yekta Anıl <YektaAnilAksoy@users.noreply.github.com>
2017-03-14 13:37:12 +0200

    Create String Task.java

---
+++ String Task.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/* Problem Link =  http://codeforces.com/problemset/problem/118/A    */
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next().toLowerCase();
+		String result="";
+		
+		for(int i = 0;i<str.length();i++){
+			if(str.charAt(i)!='a' && str.charAt(i)!='o' && str.charAt(i)!='y' && str.charAt(i)!='e'
+ && str.charAt(i)!='u'  && str.charAt(i)!='i'){
+				result+="."+str.charAt(i);
+			}
+		}
+		System.out.println(result);
+		sc.close();
+	}
+
+}
