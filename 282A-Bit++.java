2e0307a2d52c5fd259be9328d6a90087067f6deb
Yekta Anıl <YektaAnilAksoy@users.noreply.github.com>
2017-03-17 19:15:18 +0200

    Create 282A-Bit++.java

---
+++ 282A-Bit++.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.*;
+
+/* 
+
+ Problem Link = http://codeforces.com/problemset/problem/282/A
+*/
+
+public class Solution {
+	static FastReader in;
+	static PrintWriter out;
+	
+	public static void Solve(){
+       int x = 0;
+       int n = in.nextInt();
+       
+       while(n-->0){
+    	   String op = in.next();
+    	   if(op.equals("++X") || op.equals("X++"))
+    		   x++;
+    	   else
+    		   x--;
+       }
+       out.print(x);
+	}
+	
+	public static void main(String[] args) {
+		in = new FastReader();
+		out = new PrintWriter(System.out);
+		
+		//long s = System.currentTimeMillis();
+		Solve();
+		out.flush();
+		//System.out.println(System.currentTimeMillis()-s +"ms");
+	}
+	
+	
+	
+	static class FastReader
+    {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader()
+        {
+            br = new BufferedReader(new
+                     InputStreamReader(System.in));
+        }
+ 
+        String next()
+        {
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException  e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+ 
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
+
+
